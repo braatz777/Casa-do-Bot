1396411bc2ea644a7c1efd6941c93b0cd337617d
Braatz <lucasgbraatz@gmail.com>
2026-08-28 15:14:22 -0300

    ta indo

--- Coruja/demo/src/main/java/com/example/demo/BotController.java
+++ Coruja/demo/src/main/java/com/example/demo/BotController.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BotController {
+
+    private List<Resposta> respostas = new ArrayList<>();
 
     @PostMapping("/enviar")
     public String receberDados(
@@ -13,10 +15,20 @@ public class BotController {
             @RequestParam String vai,
             @RequestParam String levar) {
 
-        System.out.println(nome);
-        System.out.println(vai);
-        System.out.println(levar);
+        Resposta resposta = new Resposta(nome, vai, levar);
+
+        respostas.add(resposta);
+
+        System.out.println("Resposta adicionada!");
+        System.out.println("Nome: " + nome);
+        System.out.println("Vai: " + vai);
+        System.out.println("Levar: " + levar);
 
         return "Dados recebidos!";
+    }
+
+    @GetMapping("/respostas")
+    public List<Resposta> mostrarRespostas() {
+        return respostas;
     }
 }
